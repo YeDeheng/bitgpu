d4d1fcbe0eea542a56870ff9bfa52447135fe8e5
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-11-23 10:59:30 +0800

    add some explanation

--- prune.sh
+++ prune.sh
@@ -5,10 +5,12 @@ ERROR_THRESH=2e-4
 
 #for DESIGN in BlackScholes # poly poly6 poly8 diode level1_linear level1_satur approx1 approx2 caprasse bellido
 #for DESIGN in poly diode level1_linear bellido poly6 level1_saturation approx1 approx2 poly8 caprasse 
-for DESIGN in iir8 deriche gaussian sobel
+for DESIGN in poly #iir8 deriche gaussian sobel
 do
-	echo $DESIGN
+	echo -n "benchmark:  "
+    echo $DESIGN
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+    echo -n "search space after pruning:  "
 	./bin/prune_driver ./data/$DESIGN/$DESIGN.asm $ERROR_THRESH $UPPER_BOUND  | grep search | sed "s/.*:\(.*\)/$DESIGN \1/"
 done
 
