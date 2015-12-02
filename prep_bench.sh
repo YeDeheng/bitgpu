9319ee7acbbfa4d838aede575d7b993ffdcbfe5d
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-12-02 11:32:14 +0800

    silent mode

--- prep_bench.sh
+++ prep_bench.sh
@@ -2,9 +2,9 @@
 
 # iir8 fir32
 #for DESIGN in BlackScholes #poly poly6 poly8 diode level1_linear level1_satur approx1 approx2 caprasse BlackScholes binomial_option bellido
-for DESIGN in poly iir8 deriche gaussian sobel
+for DESIGN in poly3 # iir8 deriche gaussian sobel
 do
-	pushd ./scripts
+	pushd ./scripts &> /dev/null
 	./gimple_to_asm.sh ../bench/$DESIGN.c
-	popd
+	popd &> /dev/null
 done
