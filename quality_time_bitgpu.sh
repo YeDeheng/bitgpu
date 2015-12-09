8d6fff87643577c0a0f92c930bf538b350652a9e
Nachiket Kapre <nachiket@ieee.org>
2015-12-09 19:56:40 +1300

    done

---
+++ quality_time_bitgpu.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh -i
+
+UPPER_BOUND=25
+ERROR_THRESH=2e-4
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
+rm -f ./data/quality_time_bitgpu.dat
+for DESIGN in level1_linear poly diode bellido poly6 level1_saturation approx1 poly8 approx2 caprasse
+do
+	echo $DESIGN
+	./bin/range_driver ./data/$DESIGN/$DESIGN.asm 1 32
+	./bin/prune_driver ./data/$DESIGN/$DESIGN.asm $ERROR_THRESH $UPPER_BOUND
+	rm -f ./data/raw_bitgpu_$DESIGN.dat
+	for BLOCK in 32 64 128 256 512
+	do
+		./bin/bitgpu_driver ./data/$DESIGN/$DESIGN.asm \
+			$ERROR_THRESH \
+			1 \
+			1 \
+			$BLOCK | grep -v "good" >> ./data/raw_bitgpu_$DESIGN.dat
+	done
+	cat ./data/raw_bitgpu_$DESIGN.dat | mymin > ./data/quality_time_bitgpu_$DESIGN.dat
+	cat ./data/raw_bitgpu_$DESIGN.dat | mymin | sed "s/^/$DESIGN,/" > ./data/quality_time_bitgpu.dat
+done
